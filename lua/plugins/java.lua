c30d4bafc9f6e50d440d8d89652333e9c12ae96f
tls1235 <yotam.mini3@gmail.com>
2026-04-30 23:22:28 +0300

    s

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -1,3 +1,25 @@
 return {
   { import = "lazyvim.plugins.extras.lang.java" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "java" } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- make sure mason installs the server
+      servers = {
+        jdtls = {},
+      },
+      setup = {
+        jdtls = function()
+          return true -- avoid duplicate servers
+        end,
+      },
+    },
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = { ensure_installed = { "java-debug-adapter", "java-test" } },
+  },
 }
