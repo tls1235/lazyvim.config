a65f274fd0d2983b4a563378f3cc59350f35819a
tls1235 <yotam.mini3@gmail.com>
2026-09-24 02:26:55 +0300

    made project picker always show its absolute location

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,5 +2,15 @@ return {
   "folke/snacks.nvim",
   opts = {
     animate = { enabled = false },
+    picker = {
+      sources = {
+        projects = {
+          format = function(item, picker)
+            local path = vim.fn.fnamemodify(item.file, ":/")
+            return { { path, "SnacksPickerDirectory" } }
+          end,
+        },
+      },
+    },
   },
 }
